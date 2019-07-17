cc28b7361e14f6fcbc38f8d9db1f5bb9eb7c999e
Levi Miller <paullevimiller@gmail.com>
2019-07-16 17:17:52 -0700

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet_string)
+  tweet_array = tweet_string.split(" ").map do |word|
+    if dictionary.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word = word
+    end
+  end
+  tweet_array.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    return word_substituter(tweet)
+  else
+    return tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  shortened_tweet = word_substituter(tweet)
+  if shortened_tweet.length > 140
+    return shortened_tweet = shortened_tweet[0..136] + "..."
+  else
+    return shortened_tweet
+  end
+end
